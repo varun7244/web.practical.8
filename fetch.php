47878df1a7a13234ca05333ce696e8d5a4db1430
varun7244 <72227999+varun7244@users.noreply.github.com>
2021-10-10 15:33:46 +0530

    Update and rename welcome.php to fetch.php

---
+++ fetch.php
@@ -0,0 +1,4 @@
+<?php
+  echo "Welcome ".$_POST["fname"]."<br>";
+  echo "YOur Age is ".$_POST["age1"]."<br>";
+ ?> 
--- welcome.php
+++
@@ -1,4 +0,0 @@
-<?php
-  echo "Welcome ".$_POST["name_n"]."<br>";
-  echo "YOur Age is ".$_POST["age_1"]."<br>";
- ?> 
